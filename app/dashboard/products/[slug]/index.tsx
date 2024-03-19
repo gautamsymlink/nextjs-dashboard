01cd96b67841fb9b306564f3ba312cbf69e0a295
gautamsymlink <132981315+gautamsymlink@users.noreply.github.com>
2024-03-19 09:58:03 +0530

    slug

--- app/dashboard/products/[slug]/index.tsx
+++ app/dashboard/products/[slug]/index.tsx
@@ -2,5 +2,5 @@ import { useRouter } from 'next/router';
 
 export default function Page() {
   const router = useRouter();
-  return <p>Post: {router.query.slug}</p>;
+  return <p>Post: {router?.query?.slug}</p>;
 }
